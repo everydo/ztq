73891ad83d0522ac92dd968dd55714c5f6a0dc3a
Liang Suilong <liangsuilong@gmail.com>
2014-09-11 16:01:52 +0800

    sys.argv改为-1

--- ztq_console/ztq_console/utils/password.py
+++ ztq_console/ztq_console/utils/password.py
@@ -5,20 +5,14 @@ import os
 
 def get_password():
     cf = ConfigParser()
-    try:
-        cf.read(sys.argv[2])
-    except:
-        cf.read(sys.argv[1])
+    cf.read(sys.argv[-1])
     password_path = cf.get('password_path', 'password_path')
     cf.read(password_path)
     return cf.get('password', 'password')
 
 def modify_password(new_password):
     cf = ConfigParser()
-    try:
-        cf.read(sys.argv[2])
-    except:
-        cf.read(sys.argv[1])
+    cf.read(sys.argv[-1])
     password_path = cf.get('password_path', 'password_path')
     if os.path.exists(password_path):
         passwd_txt = ConfigParser()
