5b09166a20aed75abd85a851ff704a5e9b7f4716
Liang Suilong <liangsuilong@gmail.com>
2015-10-09 15:56:49 +0800

    改正windows控制台修改密码的方法

--- ztq_console/ztq_console/utils/password.py
+++ ztq_console/ztq_console/utils/password.py
@@ -5,14 +5,14 @@ import os
 
 def get_password():
     cf = ConfigParser()
-    cf.read(sys.argv[-1])
+    cf.read(sys.argv[1])
     password_path = cf.get('password_path', 'password_path')
     cf.read(password_path)
     return cf.get('password', 'password')
 
 def modify_password(new_password):
     cf = ConfigParser()
-    cf.read(sys.argv[-1])
+    cf.read(sys.argv[1])
     password_path = cf.get('password_path', 'password_path')
     if os.path.exists(password_path):
         passwd_txt = ConfigParser()
